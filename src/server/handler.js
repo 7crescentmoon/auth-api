e336065d241059fad6e14f5b4d6fb853bc514a9d
moon <mirfan1537.gmail.com>
2024-05-28 01:48:30 +0700

    api

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,44 +1,46 @@
-import { getsAuth } from "./configs/firebase.js";
-
-export const test = (req, res) => {
-   res.send("Hello World!")
-}
-
-export const register = async (req , res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const userRecord = await getsAuth.createUser({
-      email: email,
-      password: password,
-      displayName: name,
-    });
-
-    const user = {
-      name: name,
-      email: email,
-      uid: userRecord.uid,
-    };
-
-    await setUser(userRecord.uid, user);
-
-    res
-      .status(201)
-      .send({ message: "User registered successfully", uid: userRecord.uid });
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-}
-
-
 import { db } from "./configs/firebase.js";
+import { HTTPException } from "./utils/HTTPException.js"
 
 export const setUser = async (uid, userData) => {
   try {
     await db.collection("users").doc(uid).set(userData);
   } catch (error) {
     console.error(error);
-    throw new HTTPException(500, { message: "Internal server error" });
+    next(new HTTPException(500, { message: "Internal server error" }));
   }
 };
+
+export const test = (req, res) => {
+  res.send("Hello World!")
+}
+
+
+// export const register = async (req , res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     const userRecord = await getsAuth.createUser({
+//       email: email,
+//       password: password,
+//       displayName: name,
+//     });
+
+//     const user = {
+//       name: name,
+//       email: email,
+//       uid: userRecord.uid,
+//     };
+
+//     await setUser(userRecord.uid, user);
+
+//     res
+//       .status(201)
+//       .send({ message: "User registered successfully", uid: userRecord.uid });
+//   } catch (error) {
+//     res.status(400).send({ error: error.message });
+//   }
+// }
+
+
+
 
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { authenticateFirebaseToken } from "./middlewares/middware.js";
-import {register,test} from "./handler.js"
+import {test} from "./handler.js"
 
 const router = express.Router();
 
 router.get("/", authenticateFirebaseToken, test)
-
-router.post("/register", register);
+// router.post("/register", register);
 
 export default router;    
